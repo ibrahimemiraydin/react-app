ca20e90cbb9824730dccbfacaea14f5455570eb5
Emir Aydın <emiraydin1625@hotmail.com>
2024-11-13 20:32:27 +0300

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 // src/components/Header.js
 import React from 'react';
+import './Header.css';
 
 const Header = ({ title }) => {
   return (
-    <header style={{ padding: '20px', textAlign: 'center', background: '#282c34', color: 'white' }}>
+    <header>
       <h1>{title}</h1>
     </header>
   );
